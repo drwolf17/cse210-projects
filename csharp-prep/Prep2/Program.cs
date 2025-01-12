00bdac7cdbe61cedc708a3fb0a0712abc6d0df48
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-01-11 23:04:58 -0700

    Made the program; asks the user for their grade then prints out their letter grade.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade: ");
+        string userGrade = Console.ReadLine();
+
+        int grade = int.Parse(userGrade);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
