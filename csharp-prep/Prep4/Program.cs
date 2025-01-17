0acc8de4c4ec5db4059a002259f97f43ad6c62d7
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-01-16 23:38:41 -0700

    Made the program to ask the user for numbers then add them up average them and find the largest number.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = 1;
+        int total = 0;
+        int largest = 0;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+            numbers.Add(userNumber);
+            }
+        }
+
+        foreach (int number in numbers)
+        {
+            total += number;
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        float average = (float)total / numbers.Count;
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest is: {largest}");
+
     }
 }
