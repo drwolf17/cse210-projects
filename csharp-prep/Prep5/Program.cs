0da4b36444f586e09c2331d59350b10b53f05144
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-01-16 23:55:14 -0700

    Wrote five different functions to display a welcome message, ask for user's name, ask for user's favorite number, square a number, and display the results.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int numberSquared = SquareNumber(userNumber);
+        DisplayResult(userName, numberSquared);
+
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+    static int SquareNumber(int x)
+    {
+        x *= x;
+        return x;
+    }
+    static void DisplayResult(string userName, int x)
+    {
+        Console.WriteLine($"Hello {userName}, you favorite number squared is {x}");
     }
 }
