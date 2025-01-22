ba76687f73ac88a25b29f5c4a816d7338cacf9be
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-01-21 23:24:07 -0700

    Added the Display method to the Job class and added code in the main program to test it.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,9 +7,18 @@ class Program
         Job job1 = new Job();
         Job job2 = new Job();
 
+        job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        job2._jobTitle = "Manager";
         job2._company = "Apple";
-        Console.WriteLine(job1._company);
-        Console.WriteLine(job2._company);
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job1.Display();
+        job2.Display();
+
     }
 }
