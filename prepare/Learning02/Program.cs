3d53a4de9946214e391b932ecc386c6ecb22ff93
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-01-21 23:15:16 -0700

    Added code to the main program to test the Job class.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._company = "Microsoft";
+        job2._company = "Apple";
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
     }
 }
