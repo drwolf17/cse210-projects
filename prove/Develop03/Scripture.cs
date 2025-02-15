e2312b0257f5686eb83bc5336e737f76821c5695
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-02-14 20:59:01 -0700

    Created the RandomHide and DisplayScripture member methods in the Scripture class. RandomHide selects an amount of words to randomly hide using the MakeUnderscores member method in the ScriptureWord class. Before it hides anything it checks how many words are already hidden and if the amount inputted is more than the possible amount of words that aren't already hidden it will set the amount to the amount of visible words still left and set a new member variable made to show when all words are hidden. The DisplayScripture member method displays each word in _words.

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,11 +1,13 @@
 public class Scripture
 {
     private string _text;
+    private bool _isAllHidden;
     private List<ScriptureWord> _words = new List<ScriptureWord>();
 
     public Scripture(string text)
     {
         _text = text;
+        _isAllHidden = false;
 
         List<string> wordList = _text.Split(new char[] {' '}).ToList();
 
@@ -25,6 +27,12 @@ public class Scripture
     public void SetText(string text)
     {
         _text = text;
+        ResetWords();
+    }
+
+    public bool AreAllHidden()
+    {
+        return _isAllHidden;
     }
 
     public void ResetWords()
@@ -38,5 +46,53 @@ public class Scripture
 
             _words.Add(scriptureWord);
         }
+    }
+
+    public void RandomHide(int amount)
+    {
+        int numberShown = 0;
+
+        foreach (ScriptureWord word in _words)
+        {
+            if (word.GetIsHidden() == false)
+            {
+                numberShown++;
+            }
+        }
+
+        if (amount >= numberShown)
+        {
+            amount = numberShown;
+            _isAllHidden = true;
+        }
+
+        if (amount > 0)
+        {        
+            Random rand = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                int randomNumber = rand.Next(_words.Count());
+
+                if (_words[randomNumber].GetIsHidden() == false)
+                {
+                    _words[randomNumber].SetIsHidden(true);
+                }
+
+                else
+                {
+                    i--;
+                }
+
+            }
+        }
+    }
+
+    public void DisplayScripture()
+    {
+        foreach (ScriptureWord word in _words)
+        {
+            word.DisplayWord();
+        }
+
     }
 }
