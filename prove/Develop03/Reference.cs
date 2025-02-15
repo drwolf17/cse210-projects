c40941ab91e10c1097c146123d57165da59f95f3
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-02-14 18:43:22 -0700

    Created the Reference class to be used for storing and displaying the scripture reference and created the getters and setters for it.

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,87 @@
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _firstVerse;
+    private int _lastVerse;
+
+    public Reference()
+    {
+        _book = "";
+        _chapter = 0;
+        _firstVerse = 0;
+        _lastVerse = 0;
+    }
+
+    public Reference(string book)
+    {
+        _book = book;
+        _chapter = 0;
+        _firstVerse = 0;
+        _lastVerse = 0;
+    }
+
+    public Reference(string book, int chapter)
+    {
+        _book = book;
+        _chapter = chapter;
+        _firstVerse = 0;
+        _lastVerse = 0;
+    }
+
+        public Reference(string book, int chapter, int first)
+    {
+        _book = book;
+        _chapter = chapter;
+        _firstVerse = first;
+        _lastVerse = 0;
+    }
+
+        public Reference(string book, int chapter, int first, int last)
+    {
+        _book = book;
+        _chapter = chapter;
+        _firstVerse = first;
+        _lastVerse = last;
+    }
+
+    public string GetBook()
+    {
+        return _book;
+    }
+
+    public void SetBook(string book)
+    {
+        _book = book;
+    }
+
+        public int GetChapter()
+    {
+        return _chapter;
+    }
+
+    public void SetChapter(int chapter)
+    {
+        _chapter = chapter;
+    }
+
+        public int GetFirst()
+    {
+        return _firstVerse;
+    }
+
+    public void SetFirst(int first)
+    {
+        _firstVerse = first;
+    }
+
+        public int GetLast()
+    {
+        return _lastVerse;
+    }
+
+    public void SetLast(int last)
+    {
+        _lastVerse = last;
+    }
+}
