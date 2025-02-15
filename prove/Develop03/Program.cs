7d52cdcc6bfc72a6642ee30d925d2a69edda36f7
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-02-14 19:09:17 -0700

    Added code to test the Reference class and its member method DisplayReference.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,11 +5,16 @@ class Program
     static void Main(string[] args)
     {
         ScriptureWord testWord = new ScriptureWord("test");
+        Reference testReference1 = new Reference("Enos");
+        Reference testReference2 = new Reference("Moroni", 3);
+        Reference testReference3 = new Reference("1 Nephi", 10, 4);
+        Reference testReference4 = new Reference("Jacob", 3, 2, 5);
 
-        testWord.DisplayWord();
+        testReference1.DisplayReference();
+        testReference2.DisplayReference();
+        testReference3.DisplayReference();
+        testReference4.DisplayReference();
 
-        testWord.MakeUnderscores();
-        testWord.DisplayWord();
 
     }
 }
