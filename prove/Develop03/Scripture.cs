3f227d86491de5d2d5acacc451dda2e362ed8658
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-02-14 21:22:26 -0700

    Added code comments to make it easier to read.

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -9,6 +9,9 @@ public class Scripture
         _text = text;
         _isAllHidden = false;
 
+        // Turns the string in _text into a list of the ScriptureWord 
+        // class to seperate out each word to be able to manipulate each
+        // individually.
         List<string> wordList = _text.Split(new char[] {' '}).ToList();
 
         foreach (string word in wordList)
@@ -35,7 +38,9 @@ public class Scripture
         return _isAllHidden;
     }
 
-    public void ResetWords()
+    // Used to reset the list of words in
+    // _words if _text is updated.
+    private void ResetWords()
     {
         _words.Clear();
         List<string> wordList = _text.Split(new char[] {' '}).ToList();
@@ -48,10 +53,16 @@ public class Scripture
         }
     }
 
+    // Randomly hides an amount of words 
+    // inputted into the method (amount)
     public void RandomHide(int amount)
     {
         int numberShown = 0;
 
+        // Checking how many words aren't hidden
+        // to make sure it can actually 
+        // hide the amount of words that was
+        // inputted into the method
         foreach (ScriptureWord word in _words)
         {
             if (word.GetIsHidden() == false)
@@ -60,12 +71,24 @@ public class Scripture
             }
         }
 
+        // if there aren't enough words left to hide 
+        // it will set it so it hides all the words left
+        // and sets _isAllHidden to true so later in 
+        // the main program it will be able to check once 
+        // its finished hiding all the words.
         if (amount >= numberShown)
         {
             amount = numberShown;
             _isAllHidden = true;
         }
 
+        // if there is at least one word that
+        // it can hide it will generate a random
+        // number corresponding to one of the words
+        // in the scripture and will attempt to hide
+        // it. If the word is already hidden it will 
+        // retry and set the for loop back 1 so it 
+        // always hides the amount it needs to.
         if (amount > 0)
         {        
             Random rand = new Random();
