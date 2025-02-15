69504c741f76b2546c6788d36f2377ca3d131e97
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-02-14 22:29:47 -0700

    Added a loop that will display the scripture and scripture reference and ask the user if they want to continue or finish. After each time they press enter it hides 3 words and once they press enter while they have all words hidden or if they type in quit and hit enter it will end the loop which will end the program.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,15 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference testReference = new Reference("Jacob", 3, 2);
-        Scripture testScripture = new Scripture("O all ye that are pure in heart, lift up your heads and receive the pleasing word of God, and feast upon his love; for ye may, if your minds are firm, forever.");
+        // Sets the scripture and the reference
+        Reference reference = new Reference("Jacob", 3, 2);
+        Scripture scripture = new Scripture("O all ye that are pure in heart, lift up your heads and receive the pleasing word of God, and feast upon his love; for ye may, if your minds are firm, forever.");
 
+        // will loop for as long as there is still 
+        // words that aren't hidden and the user has not
+        // typed "quit".
+        while (true)
+        {
+            Console.Clear();
+            reference.DisplayReference();
+            scripture.DisplayScripture();
+            Console.WriteLine("");
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            
+            // Ends loop if quit is typed or all the words are hidden, placed 
+            // it here instead of having it in the while loop so it will
+            // show all the words hidden before it exits the loop and thus
+            // the program.
+            if (Console.ReadLine() == "quit" || scripture.AreAllHidden())
+            {
+                break;
+            }
 
-        testReference.DisplayReference();
-        testScripture.DisplayScripture();
-
-        testScripture.RandomHide(3);
-        testScripture.DisplayScripture();
+            else
+            {
+                scripture.RandomHide(3);
+            }
+        }
 
 
     }
