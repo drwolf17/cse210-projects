06655ac4cf70d0ca7b4f570e8ce93dd2d951f019
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-02-19 11:54:09 -0700

    Added code to the main program to test the writingAssignment class.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -7,5 +7,10 @@ class Program
         MathAssignment mathAssignment = new MathAssignment("Daniel Wolf", "Geometry", "Section 7.3", "Problems 8-19");
         Console.WriteLine(mathAssignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine(" ");
+
+        WritingAssignemnt writingAssignemnt = new WritingAssignemnt("Daniel Wolf", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignemnt.GetSummary());
+        Console.WriteLine(writingAssignemnt.GetWritingInfo());
     }
 }
