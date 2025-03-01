65f26212657d1f8490a47e40ccbbc0e837b494dc
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-01 04:34:43 -0700

    Created the BreathingMethod method which goes through the whole exercise of giving the generic greeting, asking for the duration, does the exersice for said duration (rounded up to the nearest 10) then gives the generic ending. Also fixed the BreathInOut method by moving the command that deletes the number that's counting down to after the wait so the number actually shows up and some stuff to make it look cleaner.

--- prove/Develop04/Breathing.cs
+++ prove/Develop04/Breathing.cs
@@ -3,22 +3,42 @@ public class Breathing : Mindfulness
     public Breathing() : base("Breathing", 
     "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.") {}
 
+    public void BreathingMethod()
+    {
+        GenericGreeting();
+        DurationPrompt();
+
+        Console.Clear();
+        Console.WriteLine("Get ready...");
+        WaitAnimtion(5);
+
+        int loops = (int)Math.Ceiling((double) _duration / 10);
+
+        for (int i = 0; i < loops; i++)
+        {
+            BreathInOut(5, 5);
+            Console.WriteLine("");
+        }
+
+        GenericEnding();
+    }
     public void BreathInOut(int breathIn, int breathOut)
     {
-        Console.WriteLine("Breath in...");
+        Console.WriteLine("");
+        Console.Write("Breath in...");
         for (int i = 0; i < breathIn; i++)
         {
             Console.Write(breathIn - i);
-            Console.Write("\b \b");
             Thread.Sleep(1000);
+            Console.Write("\b \b");
         }
-
-        Console.WriteLine("Breath out...");
+        Console.WriteLine("");
+        Console.Write("Breath out...");
         for (int i = 0; i < breathOut; i++)
         {
             Console.Write(breathOut - i);
-            Console.Write("\b \b");
             Thread.Sleep(1000);
+            Console.Write("\b \b");
         }
         
     }
