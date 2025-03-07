ecc521d193e4a4361806e5b50bed00bd6b557a0e
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-06 23:09:37 -0700

    In the main program made a List shapes of Shape objects and added different child objects into it then iterated through the list to print the color and area of each item.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,12 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Square square = new Square("red", 12);
-        Rectangle rectangle = new Rectangle("blue", 5, 10);
-        Circle circle = new Circle("green", 6);
+        // Square square = new Square("red", 12);
+        // Rectangle rectangle = new Rectangle("blue", 5, 10);
+        // Circle circle = new Circle("green", 6);
 
-        Console.WriteLine(square.GetArea());
-        Console.WriteLine(rectangle.GetArea());
-        Console.WriteLine(circle.GetArea());
+        // Console.WriteLine(square.GetArea());
+        // Console.WriteLine(rectangle.GetArea());
+        // Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 12));
+        shapes.Add(new Rectangle("blue", 5, 10));
+        shapes.Add(new Circle("green", 6));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.Write(shape.GetColor() + " ");
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
