b3f02917198681123c47c106605641ae0c6dd810
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-06 22:59:41 -0700

    Added code to the main program to test the Rectangle class.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,7 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Square square = new Square("red", 12);
+        Rectangle rectangle = new Rectangle("blue", 5, 10);
 
         Console.WriteLine(square.GetArea());
+        Console.WriteLine(rectangle.GetArea());
     }
 }
