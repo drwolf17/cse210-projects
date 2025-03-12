bc04ed90e4a0e9cb594eeb29ce9983955fde3659
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-11 22:30:51 -0600

    Created the SimpleGoal class as a child of the Goals class to handle simple goals that will only be completed once.

---
+++ prove/Develop05/SimpleGoal.cs
@@ -0,0 +1,37 @@
+public class SimpleGoal : Goals
+{
+    private bool _goalCompleted;
+
+    public SimpleGoal(string goalTitle, string goalDesc, int goalValue, bool goalCompleted) : base(goalTitle, goalDesc, goalValue)
+    {
+        _goalCompleted = goalCompleted;
+    }
+
+    public override void DisplayGoal()
+    {
+        if (_goalCompleted)
+        {
+            Console.Write("[X] ");
+        }
+        else
+        {
+            Console.Write("[ ] ");
+        }
+        Console.Write($"{_goalTitle} ");
+        Console.Write($"({_goalDesc})");
+    }
+
+    public override List<string> ListAttributes()
+    {
+        List<string> attributes =
+        [
+            "SimpleGoal",
+            _goalTitle,
+            _goalDesc,
+            _goalValue.ToString(),
+            _goalCompleted.ToString(),
+        ];
+
+        return attributes;
+    }
+}
