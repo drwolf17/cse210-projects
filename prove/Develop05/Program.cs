b2ac7dca7f93582fe222357190dda1d45414dbc4
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-11 23:03:08 -0600

    Added code to the main program to test the EternalGoal class.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,8 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        SimpleGoal simpleGoal = new SimpleGoal("Test title", "Test desc", 50, true);
+        SimpleGoal simpleGoal = new SimpleGoal("Simple test title", "Simple test desc", 50, true);
+        EternalGoal eternalGoal = new EternalGoal("Eternal test title", "Eternal test desc", 100, 1);
 
         simpleGoal.DisplayGoal();
+        Console.WriteLine("");
+        eternalGoal.DisplayGoal();
     }
 }
