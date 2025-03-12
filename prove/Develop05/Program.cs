bf807bff2c496c234d17c46395645f54135942af
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-11 23:15:57 -0600

    Added code to the main program to test the ChecklistGoal class.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -6,9 +6,15 @@ class Program
     {
         SimpleGoal simpleGoal = new SimpleGoal("Simple test title", "Simple test desc", 50, true);
         EternalGoal eternalGoal = new EternalGoal("Eternal test title", "Eternal test desc", 100, 1);
+        ChecklistGoal checklistGoal1 = new ChecklistGoal("Checklist test title", "Checklist test desc", 50, 100, 2, 4);
+        ChecklistGoal checklistGoal2 = new ChecklistGoal("Checklist test title", "Checklist test desc", 50, 100, 4, 4);
 
         simpleGoal.DisplayGoal();
         Console.WriteLine("");
         eternalGoal.DisplayGoal();
+        Console.WriteLine("");
+        checklistGoal1.DisplayGoal();
+        Console.WriteLine("");
+        checklistGoal2.DisplayGoal();
     }
 }
