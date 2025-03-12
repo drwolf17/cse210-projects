f6ed5bcdd8efb4d0274e8a9477c69a6f1bb3b888
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-11 22:29:32 -0600

    Created the Goals abstract class to be used as a parant class for each of the different goal types.

---
+++ prove/Develop05/Goals.cs
@@ -0,0 +1,20 @@
+public abstract class Goals
+{
+    protected string _goalTitle;
+    protected string _goalDesc;
+    protected int _goalValue;
+
+    public Goals(string goalTitle, string goalDesc, int goalValue)
+    {
+        _goalTitle = goalTitle;
+        _goalDesc = goalDesc;
+        _goalValue = goalValue;
+    }
+
+    public abstract void DisplayGoal();
+
+    // This exists for when saving classes it can just call this method
+    // for each goal and it will get a list of each of the attributes needed
+    // to create the class without having to check what the type of goal is
+    public abstract List<string> ListAttributes();
+}
