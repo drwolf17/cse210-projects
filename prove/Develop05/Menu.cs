3438ed2885de734254f36ae47702cc712464dd8a
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-14 19:00:56 -0600

    created the Menu method

---
+++ prove/Develop05/Menu.cs
@@ -0,0 +1,21 @@
+public class Menu
+{
+    private bool _isRunning;
+    private int _score;
+
+    public Menu()
+    {
+        _isRunning = true;
+        _score = 0;
+    }
+
+    public void MenuMethod()
+    {
+
+    }
+
+    public void DisplayOptions()
+    {
+        
+    }
+}
