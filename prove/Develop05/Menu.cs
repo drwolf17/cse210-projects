94aeabb678769037b8fd4a5cb010a2093ac72f82
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-16 00:29:42 -0600

    Created the CreateGoal class to prompt the user for which goal they want to make and then asks them for information and creates said goal based on what the user put in.

--- prove/Develop05/Menu.cs
+++ prove/Develop05/Menu.cs
@@ -1,6 +1,5 @@
 public class Menu
 {
-
     private int _score;
     private List<Goals> _goals = new List<Goals>();
 
@@ -13,7 +12,7 @@ public class Menu
     {
         Console.WriteLine($"You have {_score} points.");
         Console.WriteLine("");
-        Console.WriteLine("    Menu Options:");
+        Console.WriteLine("Menu Options:");
         Console.WriteLine("----------------------");
         Console.WriteLine("1. Create New Goal");
         Console.WriteLine("2. List Goals");
@@ -26,6 +25,56 @@ public class Menu
         int userSelection = int.Parse(Console.ReadLine()); 
         
         return userSelection;
+    }
+
+    public void CreateGoal()
+    {
+        Console.WriteLine("Choice of Goal:");
+        Console.WriteLine("----------------------");
+        Console.WriteLine("1. Simple Goal");
+        Console.WriteLine("2. Eternal Goal");
+        Console.WriteLine("3. Checklist Goal");
+        Console.Write(">>");
+        
+        int goalChoice = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("What is the name of your goal?");
+        Console.Write(">>");
+        string goalTitle = Console.ReadLine();
+
+        Console.WriteLine("What is a short description of your goal?");
+        Console.Write(">>");
+        string goalDesc = Console.ReadLine();
+
+        Console.WriteLine("How many points would you like it to be worth?");
+        Console.Write(">>");
+        int goalScore = int.Parse(Console.ReadLine());
+
+        if (goalChoice == 1)
+        {
+            SimpleGoal simpleGoal = new SimpleGoal(goalTitle, goalDesc, goalScore, false);
+            _goals.Add(simpleGoal);
+        }
+
+        else if (goalChoice == 2)
+        {
+            EternalGoal eternalGoal = new EternalGoal(goalTitle, goalDesc, goalScore, 0);
+            _goals.Add(eternalGoal);
+        }
+
+        else if (goalChoice == 3)
+        {
+            Console.WriteLine("How many times would you need to accomplish it to achieve the goal?");
+            Console.Write(">>");
+            int goalEnd = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many bonus points would you like when achieve the goal?");
+            Console.Write(">>");
+            int goalBonus = int.Parse(Console.ReadLine());
+
+            ChecklistGoal checklistGoal = new ChecklistGoal(goalTitle, goalDesc, goalScore, goalBonus, 0, goalEnd);
+            _goals.Add(checklistGoal);
+        }
     }
 
     public void Load(string file)
