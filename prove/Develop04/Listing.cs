709fa03f662cde134d8c3dfb1b6d5d8d80fbb3b7
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-31 13:38:57 -0600

    Created the Listing child class of the Mindfulness class and started work on the ListingMethod.

---
+++ prove/Develop04/Listing.cs
@@ -0,0 +1,35 @@
+public class Listing : Mindfulness
+{
+    private List<string> _listingPrompts;
+
+    public Listing() : base("Listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.") 
+    {
+        _listingPrompts = 
+        [
+            "Who are some people that are very important to you?",
+            "What are some positive qualities that you have?",
+            "What are some things you can do to improve?",
+            "What are some times you've felt the spirit this week?",
+            "What are some positive experiences you've felt this week?"
+        ];
+    }
+
+    public void ListingMethod()
+    {
+        GenericGreeting();
+        DurationPrompt();
+
+        GetReady();
+
+        string prompt = RandomString(_listingPrompts);
+
+        Console.WriteLine("");
+        Console.WriteLine("List as many responses as you can to the following prompt:");
+        Console.WriteLine("");
+        Console.WriteLine($"  -- {prompt} --");
+        Console.WriteLine("");
+        Console.Write("You will begin in: ");
+
+        Countdown(5);
+    }
+}
