c7d568bff617ce199a78b7e609d4ad9c31b32370
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-03-31 12:30:59 -0600

    Added some code to test the first part of the ReflectionMethod from the Reflection class.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,8 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Breathing breathing = new Breathing();
+        Reflection reflection = new Reflection();
 
-        breathing.BreathingMethod();
+        
+        reflection.ReflectionMethod();
+
     }
 }
 
