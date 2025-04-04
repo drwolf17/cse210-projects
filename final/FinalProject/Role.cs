06f4da5d0be2395cd5d544ceb5e53a37631d03b4
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-04-04 13:35:54 -0600

    Created constructor and the AddProbability() method.

--- final/FinalProject/Role.cs
+++ final/FinalProject/Role.cs
@@ -5,4 +5,16 @@ class Role
     private bool _isUnique;
     private List<double> _probabilityList;
     private List<double> _overallProbabilities;
+
+    public Role(string faction, string alignment, bool isUnique)
+    {
+        _faction = faction;
+        _alignment = alignment;
+        _isUnique = isUnique;
+    }
+
+    public void AddProbability(double probability)
+    {
+        _probabilityList.Add(probability);
+    }
 }
