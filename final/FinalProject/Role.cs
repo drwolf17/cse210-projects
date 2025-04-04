df2ce32308faddd81bb9b3316f32fb88fea796cd
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-04-04 16:34:18 -0600

    Added the _roleName attribute because I forgot I needed that.

--- final/FinalProject/Role.cs
+++ final/FinalProject/Role.cs
@@ -2,14 +2,16 @@ class Role
 {
     private string _faction;
     private string _alignment;
+    private string _roleName;
     private bool _isUnique;
     private List<double> _probabilityList;
     private List<double> _overallProbabilities;
 
-    public Role(string faction, string alignment, bool isUnique)
+    public Role(string faction, string alignment, string roleName, bool isUnique)
     {
         _faction = faction;
         _alignment = alignment;
+        _roleName = roleName;
         _isUnique = isUnique;
     }
 
