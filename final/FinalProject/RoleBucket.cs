3f2192eebc3f2ba5043cde80c3e50ce9034fc06b
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-04-05 13:41:39 -0600

    Changed the AmountUnique() virtual method to CountPossible() virtual method and added the attribute _possibleRolesAmount.

--- final/FinalProject/RoleBucket.cs
+++ final/FinalProject/RoleBucket.cs
@@ -3,21 +3,24 @@ class RoleBucket
     protected List<Role> _roles;
     protected string _alignment;
     protected int _amountUnique;
+    protected int _possibleRolesAmount;
 
     public RoleBucket(List<Role> roles, string alignment)
     {
         _roles = roles;
         _alignment = alignment;
         _amountUnique = 0;
+        _possibleRolesAmount = 0;
     }
 
-    public virtual void AmountUnique()
+    public virtual void CountPossible()
     {
         foreach (Role role in _roles)
         {
-            if (role.isUnique == true)
+            if (role.IsUnique == true)
             {
                 _amountUnique++;
+                role.AddPossible();
             }
         }
     }
