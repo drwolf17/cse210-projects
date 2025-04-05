d9c85b2866a40569a7feed533d40157fabfd3aec
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-04-04 18:26:36 -0600

    Moved some attributes over to the AllRoles class,

--- final/FinalProject/RoleList.cs
+++ final/FinalProject/RoleList.cs
@@ -1,8 +1,5 @@
 class RoleList
 {
-    private List<Role> _allRoles;
+    private AllRoles _allRoles;
     private List<RoleBucket> _roleBuckets;
-    private int _possibleCoven;
-    private int _possibleTPow;
-    private List<Role> _rolesWithProbability;
 }
