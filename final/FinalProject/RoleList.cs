2fc969a5f7e104503ad24596c34a78c66b2589a2
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-04-05 13:07:03 -0600

    Created constructor.

--- final/FinalProject/RoleList.cs
+++ final/FinalProject/RoleList.cs
@@ -2,4 +2,24 @@ class RoleList
 {
     private AllRoles _allRoles;
     private List<RoleBucket> _roleBuckets;
+
+    public RoleList(List<string> buckets)
+    {
+        _allRoles = new AllRoles();
+
+        foreach (string bucket in buckets)
+        {
+            try
+            {
+                _roleBuckets.Add(_allRoles.CreateBucket(bucket));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"{bucket} is not a possible Role Bucket.");
+                Console.WriteLine("");
+                Console.WriteLine("Remember to input the role/bucket as exactly seen");
+                Console.WriteLine("in game including spaces and capitalization.");
+            }
+        }
+    }
 }
