ba38a548a551fe97420056ddf9eb6728e0a83d0e
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-04-05 14:51:00 -0600

    Added code to the main program to test the RoleList and AllRoles classes.

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        List<string> buckets = 
+        [
+            "Any",
+            "Town Investigative",
+            "Town Investigative",
+            "Town Power",
+            "Town Power",
+            "Town Protective",
+            "Random Town",
+            "Random Town",
+            "Common Town",
+            "Common Town",
+            "Coven Power",
+            "Coven Killing",
+            "Common Coven",
+            "Random Coven",
+            "Random Neutral"
+        ];
+
+        RoleList roleList = new RoleList(buckets);
+
+        Console.WriteLine("Stop");
     }
 }
