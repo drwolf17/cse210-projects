1332f8cdb46309706dec174bd715cc3a7b2aca29
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-04-06 12:47:10 -0600

    Created the override for the AddRoleSpawnChance method.

--- final/FinalProject/Neutral.cs
+++ final/FinalProject/Neutral.cs
@@ -11,6 +11,7 @@ class Neutral : RoleBucket
                 if (_alignment == "Any" || _alignment == role.Alignment)
                 {
                     role.AddPossible();
+                    _possibleRolesInBucket++;
                     
                     if (role.IsUnique == true)
                     {
@@ -21,5 +22,26 @@ class Neutral : RoleBucket
         }
 
         return roles;
+    }
+    public override List<Role> AddRoleSpawnChance(AllRoles allRoles)
+    {
+        foreach (Role role in allRoles.Roles)
+        {
+            if (role.Faction == "Neutral" && (role.Alignment == _alignment || role.Alignment == "Any") && role.IsUnique && role.TimesConfirmed > 0)
+            {
+                _possibleRolesInBucket--;
+            }
+        }
+
+        double baseNonUniqueChance = ((double)_possibleRolesInBucket - 1)/(double)_possibleRolesInBucket;
+        foreach (Role role in allRoles.Roles)
+        {
+            if (role.Faction == "Neutral" && (role.Alignment == _alignment || role.Alignment == "Any") && (role.IsUnique == false || (role.IsUnique && role.TimesConfirmed == 0)))
+            {
+                role.AddProbability(baseNonUniqueChance);
+            }
+        }
+
+        return allRoles.Roles;
     }
 }
