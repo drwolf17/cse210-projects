ffcaaa1f73da6d344a3f334d50003d4557ee68ed
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-04-07 05:51:39 -0600

    Created the MenuMethod and Load method

--- final/FinalProject/Menu.cs
+++ final/FinalProject/Menu.cs
@@ -2,4 +2,28 @@ class Menu
 {
     private RoleList _roleList;
     private string _fileName;
+
+    public Menu() {}
+
+    public void MenuMethod()
+    {
+        Console.WriteLine("Input file name of role list you want to calculate the odds of a role appearing of");
+        Console.Write(">>");
+        _fileName = Console.ReadLine();
+
+        _roleList = new RoleList(Load());
+
+        _roleList.CountForRolesAndBuckets();
+
+        _roleList.SpawnChance();
+    }
+
+    public List<string> Load()
+    {
+        string[] lines = System.IO.File.ReadAllLines(_fileName);
+
+        List<string> buckets = [.. lines];
+
+        return buckets;
+    }
 }
