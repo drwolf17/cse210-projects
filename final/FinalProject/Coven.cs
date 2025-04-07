7a99309555b7c6bd3c0756e63ca7bae1256b8870
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-04-07 00:46:24 -0600

    Created an override for the AddRoleSpawnChance method.

--- final/FinalProject/Coven.cs
+++ final/FinalProject/Coven.cs
@@ -12,16 +12,62 @@ class Coven : RoleBucket
                 {
                     _amountUnique++;
                     role.AddPossible();
+                    _possibleRolesInBucket++;
                 }
 
                 else if (_alignment == "Common" && role.Alignment != "Power" && role.Alignment != "Killing")
                 {
                     _amountUnique++;
                     role.AddPossible();
+                    _possibleRolesInBucket++;
                 }
             }
         }
 
         return roles;
+    }
+
+    public override List<Role> AddRoleSpawnChance(AllRoles allRoles)
+    {
+        int confRoleInBucket = 0;
+        if (_alignment == "Any")
+        {
+            confRoleInBucket = allRoles.ConfirmedCoven;
+        }
+
+        foreach (Role role in allRoles.Roles)
+        {
+            if (role.Faction == "Coven")
+                if (_alignment == "Common" && role.Alignment != "Power" && role.Alignment != "Killing" && role.TimesConfirmed > 0)
+                {
+                    confRoleInBucket++;
+                }
+
+                else if (_alignment == role.Alignment && role.TimesConfirmed > 0)
+                {
+                    confRoleInBucket++;
+                }
+        }
+
+        int effectiveRolesInBucket = _possibleRolesInBucket - confRoleInBucket;
+
+        double baseNonUniqueChance = ((double)effectiveRolesInBucket - 1)/(double)effectiveRolesInBucket;
+        foreach (Role role in allRoles.Roles)
+        {
+            if (role.Faction == "Coven" && role.TimesConfirmed == 0)
+            {
+                if (role.Alignment == _alignment || _alignment == "Any" || (_alignment == "Common" && role.Alignment != "Power" && role.Alignment != "Killing"))
+                {
+                    int n = _possibleRolesInBucket - confRoleInBucket;
+                    int k = effectiveRolesInBucket;
+                    double chosenChance = Factorial(n - 1) / ((double)(Factorial(k - 1) * Factorial(n - 1 - (k - 1)))) /
+                        (Factorial(n) / (Factorial(k) * Factorial(n - k)));
+                    role.AddProbability(baseNonUniqueChance);
+                    role.AddChosen(chosenChance);
+                }
+            }
+        }
+
+        return allRoles.Roles;
     }
 }
