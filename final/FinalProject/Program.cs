b07f4a21701d81704a3958fceb5302c76b92addb
Daniel Wolf <daniel.ross.wolf@gmail.com>
2025-04-07 05:52:37 -0600

    Removed all the test stuff from main and created an instance of the Menu class and ran the method that gets everything started.

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,40 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        // List<string> buckets = 
-        // [
-        //     "Crusader",
-        //     "Town Investigative",
-        //     "Town Investigative",
-        //     "Town Power",
-        //     "Town Power",
-        //     "Town Protective",
-        //     "Random Town",
-        //     "Random Town",
-        //     "Common Town",
-        //     "Common Town",
-        //     "Coven Power",
-        //     "Coven Killing",
-        //     "Common Coven",
-        //     "Random Coven",
-        //     "Random Neutral"
-        // ];
+        Menu menu = new Menu();
 
-        List<string> allAny = 
-        [
-            "Random Town",
-            "Common Town",
-            "Common Town",
-            "Town Support",
-            "Town Power"
-        ];
-
-        RoleList roleList = new RoleList(allAny);
-
-        roleList.CountForRolesAndBuckets();
-
-        roleList.SpawnChance();
-
-        Console.WriteLine("Stop");
+        menu.MenuMethod();
     }
 }
